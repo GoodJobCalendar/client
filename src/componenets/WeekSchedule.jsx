e772ab246dcaaa89a7bf6516b466100318b68d8b
YoujungSon <andn1026@gmail.com>
2022-07-26 00:13:43 +0900

    update: 주간 캘린더 css

--- src/componenets/WeekSchedule.jsx
+++ src/componenets/WeekSchedule.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 
 const WeekSchedule = () => {
+  const dispatch = useDispatch();
   const selectDay = useSelector((state) => state.date.select);
-  const date = new Date(selectDay?.year, selectDay?.month, selectDay?.elm);
-  const Month = date.getMonth();
+  const date = selectDay
+    ? new Date(selectDay?.year, selectDay?.month - 1, selectDay?.elm)
+    : new Date();
+  const Year = date.getFullYear();
+  const Month = date.getMonth() + 1;
   const Dates = date.getDate();
   const getDay = date.getDay();
   const First = new Date(date.getFullYear() / (Month + 1) + 1);
   const monthFirstDateDay = First.getDay();
-  const weekIndex = Math.floor((Dates + monthFirstDateDay) / 7);
+  const weekIndex = Math.ceil((Dates + monthFirstDateDay) / 7);
   const DAY = ["일", "월", "화", "수", "목", "금", "토"];
+
   const Time = [
     "03:00",
     "06:00",
@@ -22,41 +27,57 @@ const WeekSchedule = () => {
     "22:00",
     "24:00",
   ];
-  // 이번주 일요일 구하기
-  function getBeginOfWeek(
-    date = new Date(selectDay?.year, selectDay?.month - 1, selectDay?.elm),
-    startOfWeek = 0
-  ) {
-    const result = new Date(date);
-    while (result.getDay() !== startOfWeek) {
-      result.setDate(result.getDate() - 1);
-    }
-    console.log(result);
-    return Number(String(result).split(" ")[2]);
-  }
-  console.log(getBeginOfWeek());
+
+  // const SelectDay = () => {
+  //   dispatch();
+  // };
 
   return (
     <>
       <Head>
         <WeekBtn>&lt;</WeekBtn>
         <WeekTitle>
-          {`${String(Month).padStart(2, "0")}월 ${weekIndex + 1}`}주차
+          {`${Year}`}년 {`${String(Month).padStart(2, "0")}월 ${weekIndex}`}
+          주차
         </WeekTitle>
         <WeekBtn>&gt;</WeekBtn>
       </Head>
+
       <WeekWrap>
-        <Days>
-          {DAY.map((elm, idx) => {
-            return <Day key={idx}>{elm}</Day>;
-          })}
-        </Days>
-        <DayList>
-          {["", "", "", "", "", "", ""].map((elm, idx) => {
-            const monday = getBeginOfWeek() + idx;
-            return <li key={idx}>{monday}</li>;
-          })}
-        </DayList>
+        <Title>
+          <Days>
+            <li></li>
+            {DAY.map((elm, idx) => {
+              return <Day key={idx} /*onClick={SelectDay}*/>{elm}</Day>;
+            })}
+          </Days>
+          <DayList>
+            <li></li>
+            {["", "", "", "", "", "", ""].map((elm, idx) => {
+              function getBeginOfWeek(
+                date = selectDay
+                  ? new Date(
+                      selectDay?.year,
+                      selectDay?.month - 1,
+                      selectDay?.elm
+                    )
+                  : new Date(),
+                startOfWeek = 0
+              ) {
+                const result = new Date(date);
+                while (result.getDay() !== startOfWeek) {
+                  result.setDate(result.getDate() - 1);
+                }
+                // dispatch();
+                return String(
+                  new Date(result.setDate(result.getDate() + idx))
+                ).split(" ")[2];
+              }
+              const monday = getBeginOfWeek();
+              return <li key={idx}>{monday}</li>;
+            })}
+          </DayList>
+        </Title>
         <div>
           <ul>
             {Time.map((elm, idx) => {
@@ -77,6 +98,8 @@ const WeekSchedule = () => {
 export default WeekSchedule;
 const WeekWrap = styled.div`
   background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0px 1.81132px 43.4717px rgba(0, 0, 0, 0.04);
 `;
 const Head = styled.div`
   display: flex;
@@ -91,6 +114,16 @@ const WeekTitle = styled.p`
 `;
 const WeekBtn = styled.button`
   color: var(--blue3);
+`;
+const Title = styled.div`
+  padding-top: 13px;
+  li {
+    width: calc(100% / 8);
+    padding: 6px 9px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 const Days = styled.div`
   display: flex;
@@ -115,44 +148,34 @@ const TimeLine = styled.div`
   border-top: 1px dotted var(--blue2);
 `;
 const Day = styled.li`
-  width: calc(100% / 7);
-  padding: 13px 17px;
   font-weight: 500;
   font-size: 12px;
   color: var(--gray3);
   text-align: center;
-  :not(:last-child) {
-    border-right: 0;
-  }
-  width: calc(100% / 7);
-  :nth-child(7n) {
+  :nth-child(7n + 1) {
     color: var(--blue3);
   }
-  :nth-child(7n + 1) {
+  :nth-child(7n + 2) {
     color: var(--point3);
   }
 `;
-const DayList = styled.ul`
+const DayList = styled.div`
   display: flex;
   background-color: #fff;
   width: 100%;
-  > li {
+  li:first-child {
     /* ${(props) =>
       props.day === props.list ? "span{color: var(--blue4)}" : ""}; */
-    padding: 13px 17px;
-    width: calc(100% / 7);
+  }
+  li:not(:first-child) {
     font-weight: 500;
     font-size: 12px;
     color: var(--gray3);
     text-align: center;
-    :not(:last-child) {
-      border-right: 0;
-    }
-    width: calc(100% / 7);
-    :nth-child(7n) {
+    :nth-child(7n + 1) {
       color: var(--blue3);
     }
-    :nth-child(7n + 1) {
+    :nth-child(7n + 2) {
       color: var(--point3);
     }
   }
