bd1824a49aed0d099cefe36c726f44bf342034ad
YoujungSon <andn1026@gmail.com>
2023-02-09 13:06:52 +0900

    refactor: styles 변경

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,49 +1,27 @@
-import { createGlobalStyle } from "styled-components";
-const GlobalStyle = createGlobalStyle`
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     list-style: none;
     text-decoration: none; 
-    color: var(--black);
+    color: ${(props) => props.theme.colors.black};
     font-family: 'Pretendard';
-
-}
-:root{
-  --gray1:#EFEFEF;
-  --gray2:#D1D1D1;
-  --gray3:#9A9A9A;
-  --gray4:#777777;
-  --black:#111111;
-
-  --blue1:#ECF1F8;
-  --blue2:#A6C9FF;
-  --blue3:#74A0E3;
-  --blue4:#3284FF;
-
-  --point1:#5BFAD3; // 민트
-  --point2:#4F32FF; // 파랑
-  --point3:#FD6E6E; // 빨강
-}
-*::-webkit-scrollbar {
+    // 스크롤바 없애기
+    ::-webkit-scrollbar {
     display: none;
   }
-  button{
+  p,span{
+    color:inherit;
+  }
+  // 버튼 초기화
+  button {
+    background-color: transparent;
     border: 0;
     cursor: pointer;
   }
-  a{
-    color: inherit;
-  }
-  input:-webkit-autofill {
-  -webkit-box-shadow: 0 0 0px 1000px #fff inset;
 }
-input:focus {
-      ::placeholder {
-        opacity: 0;
-      }
-    }
-
 `;
+
 export default GlobalStyle;
