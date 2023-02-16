75821a4a331bb32ded034477bbb194ab853ba157
YoujungSon <andn1026@gmail.com>
2023-02-16 13:30:42 +0900

    refactor: 모바일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import Router from './router/Router';
 
 import bgLeft from './assets/bg/bg_left.png';
 import bgRight from './assets/bg/bg_right.png';
 
-import Mobile from './pages/Mobile';
-
 const App = () => {
   return (
     <BrowserBg>
-      <Mobile />
+      <MobileBg>
+        <Router />
+      </MobileBg>
       <img src={bgLeft} alt='배경이미지왼쪽' />
       <img src={bgRight} alt='배경이미지오른쪽' />
     </BrowserBg>
@@ -34,4 +35,17 @@ const BrowserBg = styled.div`
     right: 0;
     bottom: 0;
   }
+`;
+const MobileBg = styled.div`
+  min-width: 375px;
+  width: 20%;
+  height: 100vh;
+  overflow-y: scroll;
+  box-shadow: 6px 0px 14px rgba(219, 219, 219, 0.39), -26px 6px 90px rgba(73, 73, 73, 0.58);
+  background-color: var(--blue1);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9;
 `;
--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import Router from './../router/Router';
+
+// 컴포넌트
+import Nav from '../components/Nav';
 
 function Mobile() {
   return (
-    <MobileBg>
-      <Router />
-    </MobileBg>
+    <MainWrap>
+      <Nav />
+      <Outlet />
+    </MainWrap>
   );
 }
 export default Mobile;
 
-const MobileBg = styled.div`
-  min-width: 375px;
-  width: 20%;
+const MainWrap = styled.div`
+  position: relative;
+  width: 100%;
   height: 100vh;
-  overflow-y: scroll;
-  box-shadow: 6px 0px 14px rgba(219, 219, 219, 0.39), -26px 6px 90px rgba(73, 73, 73, 0.58);
-  background-color: var(--blue1);
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 9;
 `;
